0a0b2ed75737f1647864db7e53014d50d5f641d7
Alonso Espinosa <alonso.espinosa@aalto.fi>
2022-02-02 17:09:41 +0200

    Since in this branch Graphs has been updated, the incidence matrix should have been fixed, there is no longer any need to apply the fix myself

--- TrafficNetworks2.jl
+++ TrafficNetworks2.jl
@@ -9,7 +9,8 @@ using Graphs,
       UnicodePlots,
       DataStructures
 
-import Base.show
+import Base.show,
+       Graphs.incidence_matrix
        #LightGraphs.LinAlg.adjacency_matrix,
        #LightGraphs.LinAlg.incidence_matrix
 
@@ -28,19 +29,17 @@ export
     mixed_stap_comb,
     # From edge_route_incidence.jl
     all_simple_paths, edge_route_incidence,
-    # From incidence_matrix_fix.jl
-    incidence_matrix,
     # From plotting.jl
     term_plot
 
-# Fix incidence matrix (not needed for more up-to-date version of LightGraphs)
-include("incidence_matrix_fix.jl")
 include("road_networks.jl")
 include("ta_solve_db.jl")
 include("ta_solve_nc.jl")
 include("stap_object.jl")
 include("mixed_ta_comb.jl")
+
 # This file imports allsimplepaths.jl (not written by me)
 include("edge_route_incidence.jl")
 include("plotting.jl")
+
 end
